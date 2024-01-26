c48c0889a26d861e869d158baa45355c9c7b765f
Juan Azabache <juan.azabache@materiagris.pe>
2024-01-26 14:45:37 -0500

    ProyectPrueba

---
+++ WebApp/Controllers/InicioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public InicioController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:15360/api");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/api/Cliente/GetCliente/1");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var Lista = JsonSerializer.Deserialize<IEnumerable<IndexModel>>(content);
+                return View("Index", Lista);
+            }
+
+            return View(new List<IndexModel>());
+        }
+    }
+}
---
+++ WebApp/Models/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class IndexModel
+    {
+        public int IdCliente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Dni { get; set; }
+        public string Telefono { get; set; }
+        public string TelefonoRef { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int Estado { get; set; }
+    }
+}
